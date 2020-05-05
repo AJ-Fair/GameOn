d1a9a7cb65081eba5511c568a804af70e2affbd1
AJ-Fair <fair.arthur.iv@gmail.com>
2020-05-05 11:27:26 -0400

    Create PostShowTile.js

---
+++ app/javascript/react/components/PostShowTile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PostShowTile = props => {
+  const {title, game, description, datetime, id, currentUser} = props
+
+  return (
+    <div>
+      <h1 className='cell small-12 title text-black'>
+        {title}
+      </h1>
+      <br />
+      <div className='cell small-12 game'>
+        <p><strong>Game:</strong>{game}</p>
+      </div>
+      <div className='cell small 12 datetime'>
+        <p><strong>{datetime}</strong></p>
+      </div>
+      <div className='cell small-12 body'>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
