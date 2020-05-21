38c3357e47648ab1560dfe952c63cae65abc112b
AJ-Fair <fair.arthur.iv@gmail.com>
2020-05-21 10:24:23 -0400

    intial commit

--- db/migrate/20200511163418_create_joined_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinedPosts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :joined_posts do |t|
-      t.belongs_to :user, null: false
-      t.belongs_to :post, null: false
-      t.timestamps null: false
-    end
-  end
-end
---
+++ db/migrate/20200521141610_create_post_roster.rb
@@ -0,0 +1,6 @@
+class CreatePostRoster < ActiveRecord::Migration[5.2]
+  def change
+    create_table :post_rosters do |t|
+    end
+  end
+end
