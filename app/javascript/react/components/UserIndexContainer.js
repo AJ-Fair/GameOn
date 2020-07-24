2262ae20f49a5eae09ab9300a7c2cb0e1f1cd665
AJ-Fair <fair.arthur.iv@gmail.com>
2020-07-24 08:52:35 -0400

    Added Fetch Request

--- app/javascript/react/components/UserIndexContainer.js
+++ app/javascript/react/components/UserIndexContainer.js
@@ -5,6 +5,26 @@ const UserIndexContainer = props => {
 const [users, setUsers] = useState([]);
 
 const [searchTerm, setSearchTerm] = useState()
+
+useEffect(() => {
+  fetch("/api/v1/users.json")
+  .then(response => {
+    if (response.ok) {
+      return response
+    } else {
+      let errorMessage = `${response.status} (${response.statusText})`,
+      error = new Error(errorMessage)
+      throw error
+    }
+  })
+  .then(response => response.json())
+  .then(usersBody => {
+    const usersArr = usersBody
+    setUsers(usersArr)
+  })
+  .catch(error => console.error(`Error in fetch: ${error.message}`))
+}, [])
+
   return (
     <h1>This is a test line</h1>
   )
