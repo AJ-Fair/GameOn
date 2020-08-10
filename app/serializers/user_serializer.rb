272fa43cd19c3770b284447f4110e21fc5c4b9bd
AJ-Fair <fair.arthur.iv@gmail.com>
2020-08-10 09:36:06 -0400

    Begun work on profile photo editing

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :profile_photo
+  attributes :id, :email, :profile_photo, :current_user
+
+  def current_user
+    scope
+  end
 
   has_many :posts
 end
