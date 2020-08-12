d3ce910c6f5f27f0fbc8e8040aed7e0e5a3e6a0f
AJ-Fair <48727141+AJ-Fair@users.noreply.github.com>
2020-08-12 08:02:08 -0400

    minor edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo])
   end
